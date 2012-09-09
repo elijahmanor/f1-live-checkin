125a2790298f8213e434770b0cf34ee4e0e8d8ab
Elijah Manor <elijah.manor@gmail.com>
2012-09-09 10:54:24 -0500

    Add some countdown logic

--- fellowshipone.js
+++ fellowshipone.js
@@ -1,8 +1,21 @@
 /*jshint eqeqeq:true, strict:true */
 /*globals jQuery:false, console:false, toastr:false */
 
+/* TODO
+ * - Update the opend/closed classes from the server
+ * - Switch out Open/Close with icons
+ * - Intercept Participants & Staff and show in dialog when click on anchor
+ * - Persist the maximum counts in localStorage
+ * - Make overall font bigger
+ * - Make the column widths more consistant
+ * - Make the countdown & refersh logic nicer looking
+ * - Remove update button and combined with the countdown UI somehow
+ */
+
 (function ( kiosk, $, undefined ) {
 	"use strict";
+
+	kiosk.updateFrequency = 10000;
 
 	kiosk.init = function() {
 		kiosk.clearTimeout();
@@ -10,10 +23,18 @@
 		kiosk.rearrange();
 
 		(function updateLoop() {
-			window.setTimeout(function() {
-				kiosk.getCounts();
-				window.setTimeout( updateLoop, 20000 );
-			}, 20000 );
+			kiosk.getCounts(function() {
+				window.setTimeout( updateLoop, kiosk.updateFrequency );
+
+				var currentTimeout = kiosk.updateFrequency / 1000;
+				window.setTimeout( function updateTimer() {
+					currentTimeout--;
+					$( "#updateStatus" ).html( currentTimeout + " seconds" );
+					if ( currentTimeout ) {
+						window.setTimeout( updateTimer, 1000 );
+					}
+				}, 1000 );
+			});
 		}());
 	};
 
@@ -31,6 +52,16 @@
 		kiosk.insertMaxParticipants();
 		kiosk.moveMinistrySelector();
 		kiosk.insertUpdateButton();
+		kiosk.updateOpenClosed();
+		kiosk.updateLiveCheckInHeader();
+	};
+
+	kiosk.updateLiveCheckInHeader = function() {
+		jQuery( ".float_left:contains('Live Check-ins')" ).removeClass( "float_left" ).append( '<span style="float: left;">Live Check-ins</span><span style="float: right;"><span id="updateStatus">0 seconds</span><span id="updateStatusAjax" style="display: none;">Updating...</span></span>' );
+	};
+
+	kiosk.updateOpenClosed = function() {
+		jQuery( ".grid a[href$='&oc=1']" ).closest( "tr" ).css( "text-decoration", "line-through" );
 	};
 
 	kiosk.moveMinistrySelector = function() {
@@ -48,7 +79,7 @@
 			var $this = $( this ),
 				text = $.trim( $this.text() );
 
-			if ( text === "" || ~text.indexOf( "Total for" ) ) {
+			if ( ~text.indexOf( "Total for" ) || $( this ).attr( "colspan" ) === "8" ) {
 				$this.closest( "tr" ).remove();
 			}
 		});
@@ -72,6 +103,10 @@
 		jQuery( ".grid th:nth-child(" + index + "), .grid td:nth-child(" + index + ")" ).remove();
 	};
 
+
+	/*
+	<span class=""></span>
+	*/
 	//<a href="currentcheckin.aspx?actdetid=994485&amp;oc=0">Close</a>
 	//<a href="currentcheckin.aspx?actdetid=994461&amp;oc=1">Open</a>
 
@@ -141,14 +176,16 @@
 		// counts[ 0 ] = Math.floor( Math.random() * 101 ) + "";
 		$( ".grid td:nth-child(" + ( columnIndex + 1 ) + ")" ).each(function() {
 			var $anchor = $( this ).find( "a" );
+			$anchor = $anchor.length ? $anchor : $( this );
 			if ( $anchor.text() !== counts[ countIndex ] ) {
-				$anchor.text( counts[ countIndex ] ).effect( "pulsate", { times: 3 }, 500 );
+				$anchor.text( counts[ countIndex ] ).end().effect( "highlight", {}, 3000 );
+				// .effect( "pulsate", { times: 3 }, 500 );
 			}
 			countIndex++;
 		});
 	};
 
-	kiosk.getCounts = function() {
+	kiosk.getCounts = function( callback ) {
 		jQuery.ajax({
 			type: "POST",
 			accepts: "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
@@ -157,6 +194,8 @@
 			data: jQuery( "#aspnetForm" ).serialize(),
 			beforeSend: function( xhr ) {
 				xhr.setRequestHeader( "X-Requested-With", { toString: function() { return ""; } } );
+				$( "#updateStatus" ).hide();
+				$( "#updateStatusAjax" ).fadeIn( "slow" );
 			},
 			success: function( data ) {
 				//toastr.warning( "My name is Inigo Montoya. You Killed my father, prepare to die!" );
@@ -164,21 +203,33 @@
 				kiosk.removeRows( $data );
 
 				$data.find( ".grid td:nth-child(" + ( $data.find( ".grid th:contains('Participant Count')").index() + 1 ) + ")" ).each(function() {
-					participants.push( $( this ).text() );
+					participants.push( $( this ).text() || "0" );
 				});
 				kiosk.updateCounts( "Participants", participants );
 
 				$data.find( ".grid td:nth-child(" + ( $data.find( ".grid th:contains('Staff Count')").index() + 1 ) + ")" ).each(function() {
-					staff.push( $( this ).text() );
+					staff.push( $( this ).text() || "0" );
 				});
 				kiosk.updateCounts( "Staff", staff );
 
-				toastr.success( "Data has been retrieved from the server", "Success" );
+				// TODO: Save off open/close status & update UI
+				$data.find( ".grid td:nth-child(" + ( $data.find( ".grid th:contains('Staff Count')").index() + 1 ) + ")" ).each(function() {
+					staff.push( $( this ).text() || "0" );
+				});
+				kiosk.updateCounts( "Staff", staff );
+				kiosk.updateOpenClosed();
+
+				toastr.success( "Data has been retrieved from the server" ); //: " + JSON.stringify( participants ), "Success" );
 				//toastr.info( "Found Participants: " + $( data ).find( ".grid th:contains('Participant Count')" ).length );
 				//toastr.info( "Found Staff: " + $( data ).find( ".grid th:contains('Staff Count')" ).length );
 			},
 			error: function( data ) {
 				toastr.error( data, "Error" );
+			},
+			complete: function() {
+				$( "#updateStatusAjax" ).hide();
+				$( "#updateStatus" ).fadeIn( "slow" );
+				if ( callback ) { callback(); }
 			}
 		});
 	};
